5f72980d7d10bb23cbd33258802139547d7f7414
neosmu <hojunglee00@naver.com>
2025-12-19 17:47:44 +0900

    [Feat] HoldPannel 상태 알려주는 텍스트 추가

--- Assets/Scripts/Dice/DiceSlot.cs
+++ Assets/Scripts/Dice/DiceSlot.cs
@@ -6,14 +6,16 @@ using UnityEngine.UI;
 public class DiceSlot : MonoBehaviour
 {
     [SerializeField] private Image faceImage;
-    [SerializeField] private Sprite[] faceSprites; 
-    [SerializeField] private Button holdButton;  
+    [SerializeField] private Sprite[] faceSprites;
+    [SerializeField] private Button holdButton;
+    [SerializeField] private GameObject holdText;
 
     private bool isHold = false;
 
     private void Start()
     {
         holdButton.onClick.AddListener(Hold);
+        UpdateHoldUI();
     }
 
     // 주사위 값에 따른 이미지 변경
@@ -25,14 +27,23 @@ public class DiceSlot : MonoBehaviour
     public void SetHold(bool hold)
     {
         isHold = hold;
+        UpdateHoldUI();
     }
 
     private void Hold()
     {
         isHold = !isHold;
+        UpdateHoldUI();
     }
+
     public bool IsHoldSelected()
     {
         return isHold;
+    }
+
+    private void UpdateHoldUI()
+    {
+        if (holdText != null)
+            holdText.SetActive(isHold);
     }
 }
